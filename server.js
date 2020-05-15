2084ef565f0e2674e021a9528a50d5ee209caa73
ZonaKyle <43191900+ZonaKyle@users.noreply.github.com>
2020-05-14 17:45:25 -0700

    Testing connection to MongoDB

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true}).then(() => console.log('DB connection success'));
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose, yo'))
